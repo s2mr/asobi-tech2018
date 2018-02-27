c52d26e8844f2a9631083e8ff492de88070e5d03
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-02-27 20:22:56 +0900

    Add StoryViewController

--- merry/merry/Modules/Story/StoryViewController.swift
+++ merry/merry/Modules/Story/StoryViewController.swift
@@ -8,29 +8,98 @@
 //
 
 import UIKit
+import RxSwift
 
 class StoryViewController: UIViewController {
 
+    fileprivate let disposeBag = DisposeBag()
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewButtomConstraint: NSLayoutConstraint!
+
+    let chats: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+//        presenter?.loadChatRoom(index: chatRoomIndex)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+//        presenter?.viewWillDisappear()
+        self.tabBarController?.tabBar.isHidden = false
     }
-    */
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+}
+
+extension StoryViewController {
+    func setupUI() {
+        tableView.separatorColor = UIColor.clear
+        tableView.estimatedRowHeight = 10000
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.allowsSelection = false
+        tableView.keyboardDismissMode = .interactive
+
+        tableView.register(UINib(nibName: "YourChatViewCell", bundle: nil), forCellReuseIdentifier: "YourChat")
+        tableView.register(UINib(nibName: "MyChatViewCell", bundle: nil), forCellReuseIdentifier: "MyChat")
+
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped))
+        tapRecognizer.cancelsTouchesInView = false // TableViewへタップイベントを流す
+        self.tableView.addGestureRecognizer(tapRecognizer)
+    }
+
+    @objc func backgroundTapped() {
+        print("backgroundTapped()")
+    }
+}
+
+extension StoryViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.chats.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let chat = self.chats[indexPath.row]
+//        if chat.isMyChat() {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "MyChat") as! MyChatViewCell
+//            cell.clipsToBounds = true
+//            // Todo: isRead
+//            cell.updateCell(text: chat.text, time: chat.time, isRead: true)
+//            return cell
+//        } else {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "YourChat") as! YourChatViewCell
+//            cell.clipsToBounds = true
+//            cell.updateCell(text: chat.text, time: chat.time)
+//            return cell
+//        }
+
+        return UITableViewCell()
+    }
+}
+
+extension StoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        //        self.bottomView.chatTextField.resignFirstResponder()
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 10
+    }
 }
