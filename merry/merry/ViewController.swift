69b9423963408220a8dcbd5807c0e0503d272f1e
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-02-27 19:19:48 +0900

    initial commit

---
+++ merry/merry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  merry
+//
+//  Created by 下村一将 on 2018/02/27.
+//  Copyright © 2018年 kazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
